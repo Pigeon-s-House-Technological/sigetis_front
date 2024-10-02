8916c57c5ddaaa4754589494d1fbb30ad2e20cc9
ErenWhite <everblanco009@gmail.com>
2024-10-02 07:24:59 -0400

    Arreglo

--- src/components/Gestion/AgregarHU.js
+++ src/components/Gestion/AgregarHU.js
@@ -1,44 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { API_BASE_URL } from '../config';
 
-const endPoint = 'http://localhost:8000/api/historiaUsuarios';
+const endPoint = `${API_BASE_URL}/historiaUsuarios`;
+function AgregarHU() {
+  const [newTask, setNewTask] = useState({ title: '', description: '' });
 
-function AgregarHU({ newTask, setNewTask, isEditMode, taskId }) {
-  const handleTitleChange = (event) => {
-    setNewTask((prev) => ({ ...prev, title: event.target.value }));
+  const handleTitleChange = (evento) => {
+    setNewTask((prev) => ({ ...prev, title: evento.target.value }));
   };
 
-  const handleDescriptionChange = (event) => {
-    setNewTask((prev) => ({ ...prev, description: event.target.value }));
+  const handleDescriptionChange = (evento) => {
+    setNewTask((prev) => ({ ...prev, description: evento.target.value }));
   };
 
   return (
     <div className="container d-flex flex-column align-items-center">
-      <h1 className="my-4">{isEditMode ? 'Editar Historia de Usuario' : 'Agregar Historia de Usuario'}</h1>
-      <form className="w-50" onSubmit={(e) => {
-        e.preventDefault();
-      }}>
+      <h1 className="my-4">Agregar historia de usuario</h1>
+      <form className="w-50" onSubmit={(e) => { e.preventDefault(); }}>
         <div className="mb-3">
           <label htmlFor="title" className="form-label">Título:</label>
-          <input
-            type="text"
-            id="title"
-            className="form-control"
-            value={newTask.title}
-            onChange={handleTitleChange}
-            required
-          />
+          <input type="text" id="title" className="form-control" value={newTask.title} onChange={handleTitleChange} required />
         </div>
         <div className="mb-3">
           <label htmlFor="description" className="form-label">Descripción:</label>
-          <textarea
-            id="description"
-            className="form-control"
-            value={newTask.description}
-            onChange={handleDescriptionChange}
-            required
-          />
+          <textarea id="description" className="form-control" value={newTask.description} onChange={handleDescriptionChange} required />
         </div>
+        <button type="submit" className="btn btn-primary">Agregar</button>
       </form>
     </div>
   );
