740bdcec371a171b672ff9bfcabf99f00f3527f8
Boris Anthony Angulo Urquieta <borisanthony03@gmail.com>
2024-10-04 13:09:17 -0400

    mostrando grupos de asignar evaluacion

--- src/components/AsignarEvaluacion/Asignar.jsx
+++ src/components/AsignarEvaluacion/Asignar.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import BotonAtras from '../General/BotonAtras';
+import { FaPlus } from 'react-icons/fa';
 
 import { API_BASE_URL } from '../config';  // Importamos la URL de la API
 import './Asignar.css';  // Importamos los estilos CSS
@@ -9,6 +10,7 @@ import './Asignar.css';  // Importamos los estilos CSS
 const Asignar = () => {
   const { tipo } = useParams();
   const { destinatario } = useParams();
+  const { id_tutor } = 1;
 
   const destinatarioVar = destinatario === 'grupal' ? 'Grupal' : 'Individual';
   const tipoVar = tipo === '1' ? 'Autoevaluación' : tipo === '2' ? 'Evaluación Cruzada' : 'Evaluación de Pares';
@@ -18,6 +20,7 @@ const Asignar = () => {
   const [ grupos, setGrupos ] = useState([]);  // Estado para manejar los grupos
   const [ evaluaciones, setEvaluaciones ] = useState([]); // Estado para manejar las evaluaciones
   const [evaluacionSeleccionada, setEvaluacionSeleccionada] = useState('');
+  const [mensaje, setMensaje] = useState('');
   
   const obtenerEvaluaciones = async () => {
     try {
@@ -34,6 +37,16 @@ const Asignar = () => {
     }
   };
 
+  const obtnerGrupos = async () => {
+    try {
+      const response = await axios.get(`${API_BASE_URL}/grupos`); // Reemplaza con la URL de tu API
+      const gruposFiltrados = response.data.filter(grupo => grupo.id_tutor === id_tutor);
+      setGrupos(response.data);
+    } catch (error) {
+      console.error('Error al obtener los datos de la API', error);
+    }
+  }
+
   const toggleGroup = (groupIndex) => {
     if (expandedGroup === groupIndex) {
       setExpandedGroup(null);  // Colapsa el grupo si ya está expandido
@@ -42,40 +55,24 @@ const Asignar = () => {
     }
   };
 
-  // Datos simulados de los grupos
-  const gruposEjemplo = [
-    {
-      name: 'Grupo 1',
-      description: 'Menu description.',
-      members: [
-        { name: 'Alan Meneces F.', leader: false },
-        { name: 'Ever Blanco V.', leader: false },
-        { name: 'Paola Soto', leader: false },
-        { name: 'Boris Anthony U.', leader: true },
-        { name: 'Jhoanson', leader: false }
-      ]
-    },
-    {
-      name: 'Grupo 2',
-      description: 'Menu description.',
-      members: []
-    },
-    {
-      name: 'Grupo 3',
-      description: 'Menu description.',
-      members: []
-    }
-  ];
-
   useEffect(() => {
-    setGrupos(gruposEjemplo);
+    obtnerGrupos();
     obtenerEvaluaciones();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const handleEvaluacionChange = (event) => {//para la lista deslizable de evaluaciones
     setEvaluacionSeleccionada(event.target.value);
-    console.log(event.target.value);
+    setMensaje('');
+  };
+
+  const handleAddMemberClick = () => {
+    if (!evaluacionSeleccionada) {
+      setMensaje('Debe seleccionar una evaluación.');
+    } else {
+      // Lógica para agregar miembro
+      console.log('Agregar miembro');
+    }
   };
 
   return (
@@ -93,17 +90,21 @@ const Asignar = () => {
           </option>
         ))}
       </select>
-
+      {mensaje && <p className="mensaje-advertencia">{mensaje}</p>}
       {grupos.map((group, index) => (
         <div key={index} className="group">
+          
           <div className="group-header" onClick={() => toggleGroup(index)}>
-            <span>{group.name}</span>
+            <span>{group.nombre_grupo}</span>
             <span>{expandedGroup === index ? '▲' : '▼'}</span>
           </div>
           {expandedGroup === index && (
             <div className="group-details">
-              <p>{group.description}</p>
-              {group.members.length > 0 ? (
+              <button className="add-member-btn" onClick={handleAddMemberClick}>
+                <FaPlus /> {/* Icono de + */}
+              </button>
+              <p>{group.descripcion_grupo}</p>
+              {(group.members && group.members.length > 0) ? (
                 <div className="members">
                   <h4>Integrantes</h4>
                   <ul>
