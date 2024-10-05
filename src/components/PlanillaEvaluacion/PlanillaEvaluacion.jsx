8b3609fe50e5ea66e006a5972c81abb20b4354c5
Boris Anthony Angulo Urquieta <borisanthony03@gmail.com>
2024-10-05 19:56:17 -0400

    generando archivos para Planilla de evaluacion

---
+++ src/components/PlanillaEvaluacion/PlanillaEvaluacion.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import { Modal, Button, Form, Alert } from 'react-bootstrap';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import { API_BASE_URL } from '../config';
+
+import RatingCircles from '../RatingCircles';
+
+const PlanillaEvaluacion = ({ show, handleClose, fetchPreguntas }) => {
+
+}
+
+export default PlanillaEvaluacion;
