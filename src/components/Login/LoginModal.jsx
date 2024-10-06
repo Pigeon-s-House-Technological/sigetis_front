732da0876090668b8f28e385314ff031fa71bb54
samuel <jdguzman.0002@gmail.com>
2024-10-06 03:49:30 -0400

    Iniciar sesion

---
+++ src/components/Login/LoginModal.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import LoginForm from './LoginForm';
+import './Style/Login.css';
+
+const LoginModal = () => {
+    const [userType, setUserType] = useState('teacher'); // Mostrar Docente por defecto
+
+    const toggleUserType = (type) => {
+        setUserType(type);
+    };
+
+    return (
+        <div className="login-container">
+            <h2 className="title">Iniciar Sesión</h2>
+
+            <div className="toggle-buttons">
+                <button
+                    className={`toggle-button ${userType === 'teacher' ? 'active' : ''}`}
+                    onClick={() => toggleUserType('teacher')}>
+                    Docente/Tutor
+                </button>
+                <button
+                    className={`toggle-button ${userType === 'student' ? 'active' : ''}`}
+                    onClick={() => toggleUserType('student')}>
+                    Estudiante
+                </button>
+            </div>
+
+            <div className="form-container">
+                <LoginForm userType={userType} />
+            </div>
+        </div>
+    );
+};
+
+export default LoginModal;
+
+
