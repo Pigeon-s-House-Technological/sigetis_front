35056ce70999183959396ff246188d1888d66196
Boris Anthony Angulo Urquieta <borisanthony03@gmail.com>
2024-10-22 22:52:30 -0400

    realizando agragar tarea y editar tarea y eliminar tarea

--- src/components/GestionTareas/DetalleHistoria.js
+++ src/components/GestionTareas/DetalleHistoria.js
@@ -1,22 +1,26 @@
 import React, { useState, useEffect } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Dropdown } from 'react-bootstrap';
 import axios from 'axios';
-import AgregarTarea from './AgregarTarea';
-import AsignarUsuario from './AsignarUsuario';
 import { API_BASE_URL } from '../config';
 
-const endPoint = `${API_BASE_URL}/actividades`; 
+import ModalEliminar from '../General/Modales/ModalEliminar';
+import BotonAtras from '../General/BotonAtras';
+import './estilos.css'
+import AgregarTarea from './AgregarTarea';
+import AsignarUsuario from './AsignarUsuario';
+
 
 function DetalleHistoria() {
   const { id } = useParams();
-  const navigate = useNavigate();
   const [historia, setHistoria] = useState(null);
   const [tasks, setTasks] = useState([]);
+  const [idEliminar, setIdEliminar] = useState(null);
   const [showTaskModal, setShowTaskModal] = useState(false);
   const [currentTask, setCurrentTask] = useState(null);
-  const [showAsignarModal, setShowAsignarModal] = useState(false);
+  const [showEditTaskModal, setShowEditTaskModal] = useState(false);
+  const [showConfirmModal, setShowConfirmModal] = useState(false);
 
   // Fetch historia
   const fetchHistorias = async () => {
@@ -31,15 +35,19 @@ function DetalleHistoria() {
   // Fetch tasks
   const fetchTasks = async () => {
     try {
-      const response = await axios.get(`${endPoint}?id_hu=${id}`); // Asegúrate de que la API acepte este parámetro
+      const response = await axios.get(`${API_BASE_URL}/actividades`); // Asegúrate de que la API acepte este parámetro
       if (Array.isArray(response.data)) {
-        setTasks(response.data);
+        setTasks(response.data.map(task => ({
+          ...task,
+          estado_actividad: task.estado_actividad || '' // Asegurarse de que no sea null
+        })));
       } else {
         console.error("La respuesta de la API no es un array:", response.data);
         setTasks([]);
       }
     } catch (error) {
       console.error("Error al obtener las tareas:", error.response ? error.response.data : error.message);
+      setTasks([]);
     }
   };
 
@@ -52,7 +60,7 @@ function DetalleHistoria() {
     try {
       const response = await axios({
         method,
-        url: taskId ? `${endPoint}/${taskId}` : endPoint,
+        url: taskId ? `${API_BASE_URL}/actividades/${taskId}` : `${API_BASE_URL}/actividades`,
         data: task,
       });
       return response.data;
@@ -61,93 +69,100 @@ function DetalleHistoria() {
       throw new Error('Error en la operación de tarea');
     }
   };
-  
-  const addTask = async (task) => {
-    const method = currentTask ? 'PUT' : 'POST';
-    const taskId = currentTask ? currentTask.id : null;
-  
-    try {
-      const updatedTask = await handleTaskResponse(method, task, taskId);
-      setTasks((prevTasks) => {
-        if (currentTask) {
-          return prevTasks.map((t) => (t.id === updatedTask.id ? updatedTask : t));
-        }
-        return [...prevTasks, updatedTask];
-      });
-      fetchTasks(); // Actualiza las tareas desde el servidor después de agregar/editar
-    } catch (error) {
-      console.error(error.message);
-    }
-  
-    setCurrentTask(null);
-    setShowTaskModal(false);
-  };
-  
-
-  const deleteTask = async (taskId) => {
-    try {
-      await handleTaskResponse('DELETE', null, taskId);
-      setTasks(tasks.filter(task => task.id !== taskId));
-    } catch (error) {
-      console.error('Error:', error.message);
-    }
-  };
-
-  const handleAsignarClick = (task) => {
-    setCurrentTask(task);
-    setShowAsignarModal(true);
-  };
-
-  const handleAsignarClose = () => {
-    setShowAsignarModal(false);
-  };
-
-  const handleAsignarUsuario = async (usuario) => {
-    if (currentTask) {
-        // Actualiza el estado local para mostrar el nombre del usuario asignado
-        const updatedTasks = tasks.map((task) =>
-            task.id === currentTask.id ? { ...task, assigned: `${usuario.nombre_user} ${usuario.apellido_user}` } : task
-        );
-        setTasks(updatedTasks);
-
-        // Actualiza la actividad en la base de datos usando el ID del usuario a través del método PATCH
-        try {
-            await axios.patch(`${API_BASE_URL}/actividadesP/${currentTask.id}`, {
-                id_hu: currentTask.id_hu, // Suponiendo que `id_hu` es un campo requerido
-                nombre_actividad: currentTask.nombre_actividad, // Suponiendo que `nombre_actividad` es requerido
-                estado_actividad: currentTask.estado_actividad, // Suponiendo que `estado_actividad` es requerido
-                fecha_inicio: currentTask.fecha_inicio, // Suponiendo que `fecha_inicio` es requerido
-                fecha_fin: currentTask.fecha_fin, // Suponiendo que `fecha_fin` es requerido
-                encargado: currentTask.encargado, // Suponiendo que `encargado` es requerido
-                id_usuario: usuario.id, // Aquí se guarda el ID del usuario
-            });
-        } catch (error) {
-            console.error('Error al asignar el usuario:', error.response ? error.response.data : error.message);
-            // Opcional: revertir el cambio en el estado local si la API falla
-            const revertedTasks = tasks.map((task) =>
-                task.id === currentTask.id ? { ...task, assigned: null } : task
-            );
-            setTasks(revertedTasks);
-        }
-    }
-    setShowAsignarModal(false);
-  };
 
   if (!historia || !historia.titulo_hu) {
     return <div>Error: Historia no encontrada o no tiene título</div>;
   }
 
-  return (
-    <div className="container mt-5" style={{ backgroundColor: "#215F88" }}>
-      <button className="btn btn-secondary" onClick={() => navigate(-1)} style={{ backgroundColor: '#09DDCC', color: "black" }}>Volver</button>
-      <h1 className="text-center" style={{ color: 'white' }}>{historia.titulo_hu || 'Historia de Usuario'}</h1>
+  const crearClick = () => {
+    setShowTaskModal(true);
+  };
 
-      <div className="card p-3 mb-3">
-        <h2>Tareas</h2>
-        <button className="btn btn-primary" onClick={() => {
-          setCurrentTask(null);
-          setShowTaskModal(true);
-        }} style={{ backgroundColor: '#245F88' }}>Agregar Tarea</button>
+  const editarClick = (task) => {
+    setCurrentTask(task);
+    setShowEditTaskModal(true);
+  };
+
+  const eliminarClick = (id) => {
+    setIdEliminar(id);
+    setShowConfirmModal(true);
+  };
+
+  const handleSave = async (datos) => {
+    if(datos.estado === 'pendiente'){
+      datos.estado = 1;
+    }else if(datos.estado === 'en_progreso'){
+      datos.estado = 2;
+    }else{
+      datos.estado = 3;
+    }
+    try{
+      const response = await axios.post(`${API_BASE_URL}/actividades`, {
+        nombre_actividad: datos.nombre,
+        estado_actividad: datos.estado,
+        fecha_inicio: datos.fecha_inicio,
+        fecha_fin: datos.fecha_fin,
+        id_hu: id
+      });
+      console.log ('Tarea guardada:', response.data.actividad);
+      setTasks([...tasks, response.data.actividad]);
+    }catch(error){
+      console.error('Error al guardar la tarea:', error);
+    }
+  }
+
+  const handleEditSave = async(datos) => {
+    if(datos.estado === 'pendiente'){
+      datos.estado = 1;
+    }else if(datos.estado === 'en_progreso'){
+      datos.estado = 2;
+    }else{
+      datos.estado = 3;
+    }
+    try{
+      const response = await axios.patch(`${API_BASE_URL}/actividadesP/${currentTask.id}`, {
+        nombre_actividad: datos.nombre,
+        estado_actividad: datos.estado,
+        fecha_inicio: datos.fecha_inicio,
+        fecha_fin: datos.fecha_fin,
+      });
+      console.log ('Tarea editada correctamente:', response.data.actividad);
+      setTasks(tasks.map(task => (task.id === currentTask.id ? response.data.actividad : task)));
+    }catch(error){
+      console.error('Error al guardar la tarea:', error);
+    }
+  };
+
+  const handleAsignar = () => {
+  };
+
+  const handleConfirmDelete = async () => {
+    const id = idEliminar;
+    try{
+      await axios.delete(`${API_BASE_URL}/actividades/${id}`);
+      setTasks(tasks.filter(task => task.id !== id));
+      console.log('Tarea eliminada correctamente');
+      setShowConfirmModal(false)
+    }catch(error){
+      console.error('Error al eliminar la tarea:', error);
+    }
+  };
+
+  return (
+    <div className="container" style={{  }}>
+      
+      <h2 className="text-center" style={{ color: 'black' }}>Actividades</h2>
+
+      <div className="">
+        <div className="d-flex align-items-center justify-content-between">
+          <h3>{historia.titulo_hu || 'Historia de Usuario'}</h3>
+          <BotonAtras />
+        </div>
+        <div className="d-flex justify-content-center mt-3">
+          <button className="btn btn-primary" onClick={() => {crearClick();}} 
+          style={{ backgroundColor: '#007BFF' }}>Agregar Tarea
+          </button>
+        </div>
         <table className="table mt-3">
           <thead>
             <tr>
@@ -165,21 +180,33 @@ function DetalleHistoria() {
               <tr key={task.id}>
                 <td>{task.nombre_actividad}</td>
                 <td>
-                  <button className="btn btn-secondary" onClick={() => handleAsignarClick(task)}>
-                    {task.assigned}
+                  <button className="btn btn-secondary" onClick={() => handleAsignar()}>
+                    {task.encargado ? task.encargado : 'Asignar'}
                   </button>
                 </td>
                 <td>
                   <select
                     value={task.estado_actividad}
-                    onChange={(e) => {
+                    onChange={async (e) => {
                       const updatedTask = { ...task, estado_actividad: e.target.value };
+      
+                      // Actualizar la tarea en el estado local
                       setTasks(tasks.map((t) => (t.id === task.id ? updatedTask : t)));
+
+                      // Realizar la solicitud PATCH para actualizar la tarea en la base de datos
+                      try {
+                        await axios.patch(`${API_BASE_URL}/actividadesP/${task.id}`, {
+                          estado_actividad: e.target.value,
+                        });
+                        console.log('Estado de la tarea actualizado en la base de datos');
+                      } catch (error) {
+                        console.error('Error al actualizar el estado de la tarea:', error);
+                      }
                     }}
                   >
-                    <option value="pendiente">Pendiente</option>
-                    <option value="en_progreso">En progreso</option>
-                    <option value="completada">Completada</option>
+                    <option value="1">Pendiente</option>
+                    <option value="2">En progreso</option>
+                    <option value="3">Completada</option>
                   </select>
                 </td>
                 <td>{task.fecha_inicio}</td>
@@ -190,10 +217,9 @@ function DetalleHistoria() {
                     <Dropdown.Toggle variant="link" id="dropdown-basic">•••</Dropdown.Toggle>
                     <Dropdown.Menu>
                       <Dropdown.Item onClick={() => {
-                        setCurrentTask(task);
-                        setShowTaskModal(true);
+                        editarClick(task);
                       }}>Editar</Dropdown.Item>
-                      <Dropdown.Item onClick={() => deleteTask(task.id)}>Eliminar</Dropdown.Item>
+                      <Dropdown.Item onClick={() => eliminarClick(task.id)}>Eliminar</Dropdown.Item>
                     </Dropdown.Menu>
                   </Dropdown>
                 </td>
@@ -203,27 +229,28 @@ function DetalleHistoria() {
         </table>
       </div>
 
-      {showTaskModal && (
-        <div className="modal-container">
-          <AgregarTarea
-            show={showTaskModal}
-            onHide={() => setShowTaskModal(false)}
-            addTask={addTask}
-            currentTask={currentTask}
-          />
-        </div>
-      )}
-
-      {showAsignarModal && (
-        <div className="modal-container">
-          <AsignarUsuario
-            show={showAsignarModal}
-            onHide={handleAsignarClose}
-            handleAsignarUsuario={handleAsignarUsuario}
-            currentTask={currentTask}
-          />
-        </div>
-      )}
+      <AgregarTarea
+        show={showTaskModal}
+        onHide={() => setShowTaskModal(false)}
+        currentTask={currentTask}
+        handleSave={handleSave}
+        titulo={"Agregar Actividad"}
+        isEditMode={false}
+      />  
+      <ModalEliminar
+        show={showConfirmModal}
+        onClose={() => setShowConfirmModal(false)}
+        handleConfirmDelete={handleConfirmDelete}
+        titulo={"esta actividad"}
+      />
+      <AgregarTarea
+        show={showEditTaskModal}
+        onHide={() => setShowEditTaskModal(false)}
+        currentTask={currentTask}
+        handleSave={handleEditSave}
+        titulo={"Agregar Actividad"}
+        isEditMode={true}
+      /> 
     </div>
   );
 }
