c206f72f381fca9ddc3b21f8b46f4bf4d277666f
Paola <sotopaola690@gmail.com>
2024-10-27 07:53:36 -0400

    Resolviendo bugs en regitro docente y estudiante

--- src/components/RegistroEstudiante/RegistroEstudiante.js
+++ src/components/RegistroEstudiante/RegistroEstudiante.js
@@ -1,56 +1,100 @@
 import React, { useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Form, Button } from 'react-bootstrap';
+import { Form, Button, InputGroup } from 'react-bootstrap';
 import axios from 'axios';
 import { API_BASE_URL } from '../config'; 
+import { FaEye, FaEyeSlash } from 'react-icons/fa'; // Iconos para mostrar/ocultar contraseña
 
 const endPoint = `${API_BASE_URL}/usuarios`; 
 
 function RegistroEstudiante() {
-  // Definir los estados que manejarán los datos del formulario
   const [nombre_user, setNombre] = useState('');
   const [apellido_user, setApellido] = useState('');
   const [correo, setCorreo] = useState('');
   const [error, setError] = useState('');  
   const [exito, setExito] = useState(false);  
-  //const [usuario, setUsuario] = useState();  
-  //const [contrasena_usuario, setContrasena] = useState();  
+  const [usuario, setUsuario] = useState();  
+  const [contrasena_usuario, setContrasena] = useState(''); 
+  const [confirmarContrasena, setConfirmarContrasena] = useState('');
+ 
+  const [showPassword, setShowPassword] = useState(false); // Mostrar/ocultar contraseña
+  const [validationStatus, setValidationStatus] = useState({
+    length: false,
+    uppercase: false,
+    lowercase: false,
+    number: false,
+    specialChar: false
+  });
 
+  // Función para validar la contraseña
+  const validatePassword = (password) => {
+    const minLength = password.length >= 8;
+    const hasUppercase = /[A-Z]/.test(password);
+    const hasLowercase = /[a-z]/.test(password);
+    const hasNumber = /[0-9]/.test(password);
+    const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+
+    setValidationStatus({
+      length: minLength,
+      uppercase: hasUppercase,
+      lowercase: hasLowercase,
+      number: hasNumber,
+      specialChar: hasSpecialChar
+    });
+
+    // Verificar si cumple con todos los requisitos
+    if (!minLength || !hasUppercase || !hasLowercase || !hasNumber || !hasSpecialChar) {
+      //setError('La contraseña debe tener al menos 8 caracteres, una mayúscula, una minúscula, un número y un carácter especial.');
+      return false;
+    }
+
+    setError(null);
+    return true;
+  };
+
+  const handlePasswordChange = (e) => {
+    const newPassword = e.target.value;
+    setContrasena(newPassword);
+    validatePassword(newPassword);
+  };
 
   // Función para guardar un nuevo estudiante
   const handleSave = async (e) => {
-    e.preventDefault(); // 
+    e.preventDefault();
 
     try {
-      // Verificar que todos los campos estén llenos
       if (!nombre_user || !apellido_user || !correo) {
-        setError('Todos los campos son requeridos'); // Mostrar mensaje de error si algún campo está vacío
+        setError('Todos los campos son requeridos');
         return;
       }
 
-      // Datos que se enviarán al backend
+      if (contrasena_usuario !== confirmarContrasena) {
+        setError('Las contraseñas no coinciden');
+        window.scrollTo(0, 0);
+        return;
+      }
+  
+
       const data = {
-        nombre_user: nombre_user,
-        apellido_user: apellido_user,
-        correo: correo,
+        nombre_user,
+        apellido_user,
+        correo,
         tipo_usuario: 1, 
+        contrasena_usuario, // Aquí enviamos la contraseña validada
+        confirmarContrasena
+
       };
 
       console.log('Datos enviados:', data);
-      
-      
       const response = await axios.post(endPoint, data);
 
-      // Si la solicitud es exitosa, mostrar mensaje de éxito y limpiar los campos
       setExito(true);
-      setError('');  
-      setNombre('');  
-      setApellido('');  
-      setCorreo('');  
+      setError('');
+      setNombre('');
+      setApellido('');
+      setCorreo('');
+      setContrasena('');
     } catch (error) {
-      console.error('Error al guardar el estudiante:', error.response ? error.response.data : error.message);
-      
-      // Mostrar mensaje de error detallado basado en la respuesta del servidor
       if (error.response) {
         setError(`Error al registrar el estudiante: ${error.response.data.message || error.response.data}`);
       } else {
@@ -64,13 +108,9 @@ function RegistroEstudiante() {
     <div className="container mt-5" style={{ maxWidth: '500px', backgroundColor: '#F8F9FA', padding: '20px', borderRadius: '10px' }}>
       <h2 className="text-center" style={{ color: '#007BFF' }}>Registro de Estudiante</h2>
 
-      {/* Mostrar mensaje de error si existe */}
       {error && <p style={{ color: 'red' }}>{error}</p>}
-      
-      {/* Mostrar mensaje de éxito si el registro fue exitoso */}
       {exito && <p style={{ color: 'green' }}>Registro exitoso</p>}
 
-      {/* Formulario para registrar el estudiante */}
       <Form onSubmit={handleSave}>
         <Form.Group className="mb-3" controlId="formNombre">
           <Form.Label>Nombre</Form.Label>
@@ -79,12 +119,11 @@ function RegistroEstudiante() {
             placeholder="Ingresa tu nombre"
             value={nombre_user}
             onChange={(e) => {
-            const regex = /^[a-zA-Z\s]*$/; // Expresión regular que permite solo letras y espacios
-            if (regex.test(e.target.value)) {
-              setNombre(e.target.value); // Actualiza el estado solo si la entrada es válida
-            }
-          }}
-          
+              const regex = /^[a-zA-Z\s]*$/;
+              if (regex.test(e.target.value)) {
+                setNombre(e.target.value);
+              }
+            }}
           />
         </Form.Group>
 
@@ -95,23 +134,26 @@ function RegistroEstudiante() {
             placeholder="Ingresa tu apellido"
             value={apellido_user}
             onChange={(e) => {
-              const regex = /^[a-zA-Z\s]*$/; // Expresión regular que permite solo letras y espacios
+              const regex = /^[a-zA-Z\s]*$/;
               if (regex.test(e.target.value)) {
-                setApellido(e.target.value); // Actualiza el estado solo si la entrada es válida
+                setApellido(e.target.value);
               }
-            }}          />
+            }}
+          />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formCorreo">
           <Form.Label>Correo Electrónico</Form.Label>
           <Form.Control
             type="email"
-            placeholder="Ingresa tu correo electrónico"
+            placeholder="Ingresa tu correo electronico"
             value={correo}
             onChange={(e) => setCorreo(e.target.value)}
+            style={{ '::placeholder': { fontSize: '10px', color: '#999' } }}
+
           />
         </Form.Group>
-{/*
+
         <Form.Group controlId="formUsuario" className="mb-3">
           <Form.Label>Usuario</Form.Label>
           <Form.Control
@@ -125,16 +167,43 @@ function RegistroEstudiante() {
 
         <Form.Group controlId="formContrasena" className="mb-3">
           <Form.Label>Contraseña</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Ingresa tu contraseña"
-            value={contrasena_usuario}
-            onChange={(e) => setContrasena(e.target.value)}
-            required
-          />
-        </Form.Group>*/}
+          <InputGroup>
+            <Form.Control
+              type={showPassword ? 'text' : 'password'}
+              placeholder="Ingresa tu contraseña"
+              value={contrasena_usuario}
+              onChange={handlePasswordChange}
+            />
+            {/* Cambiar el estilo del InputGroup.Text para que no parezca un botón */}
+            <InputGroup.Text 
+              onClick={() => setShowPassword(!showPassword)} 
+              style={{
+                backgroundColor: 'transparent',
+                border: 'none',
+                cursor: 'pointer',
+                padding: '0 10px',
+                display: 'flex',
+                alignItems: 'center'
+              }}
+            >
+              {showPassword ? <FaEyeSlash /> : <FaEye />}
+            </InputGroup.Text>
 
-        {/*
+          </InputGroup>
+
+          {/* Criterios de validación */}
+          <ul style={{ listStyleType: 'none', paddingLeft: '0', color: 'red' }}>
+            <li style={{ color: validationStatus.length ? 'green' : 'red' }}>Al menos 8 caracteres</li>
+            <li style={{ color: validationStatus.uppercase ? 'green' : 'red' }}>Al menos una letra mayúscula</li>
+            <li style={{ color: validationStatus.lowercase ? 'green' : 'red' }}>Al menos una letra minúscula</li>
+            <li style={{ color: validationStatus.number ? 'green' : 'red' }}>Al menos un número</li>
+            <li style={{ color: validationStatus.specialChar ? 'green' : 'red' }}>Al menos un carácter especial (@, #, $, etc.)</li>
+          </ul>
+
+          {/* Mensaje de error si la contraseña no cumple con los requisitos */}
+          {/*error && <p style={{ color: 'red' }}>{error}</p>*/}
+        </Form.Group>
+
         <Form.Group controlId="formConfirmarContrasena" className="mb-3">
           <Form.Label>Confirmar Contraseña</Form.Label>
           <Form.Control
@@ -145,7 +214,6 @@ function RegistroEstudiante() {
             required
           />
         </Form.Group>
-        */}
 
         <Button variant="primary" type="submit" className="w-100">
           Registrar
--- src/components/RegistroTutor/RegistroDocente.js
+++ src/components/RegistroTutor/RegistroDocente.js
@@ -1,98 +1,159 @@
 import React, { useState } from "react";
-import { Button, Form, Container, Alert } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Form, Button, InputGroup } from 'react-bootstrap';
 import axios from 'axios';
-import { Navigate } from 'react-router-dom';
+import { API_BASE_URL } from '../config'; 
+import { FaEye, FaEyeSlash } from 'react-icons/fa'; // Iconos para mostrar/ocultar contraseña
 
-import { API_BASE_URL } from '../config';
+const endPoint = `${API_BASE_URL}/usuarios`; 
 
-
-function RegistroDocente() {
-  const [nombres, setNombres] = useState('');
-  const [apellidos, setApellidos] = useState('');
+function RegistroEstudiante() {
+  const [nombre_user, setNombre] = useState('');
+  const [apellido_user, setApellido] = useState('');
   const [correo, setCorreo] = useState('');
-  const [usuario, setUsuario] = useState('');
-  const [contrasena, setContrasena] = useState('');
+  const [error, setError] = useState('');  
+  const [exito, setExito] = useState(false);  
+  const [usuario, setUsuario] = useState();  
+  const [contrasena_usuario, setContrasena] = useState(''); 
   const [confirmarContrasena, setConfirmarContrasena] = useState('');
-  const [redirect, setRedirect] = useState(false);
-  const [error, setError] = useState(null);
-  const [showConfirmation, setShowConfirmation] = useState(false); // Variable para mostrar mensaje de confirmación
+ 
+  const [showPassword, setShowPassword] = useState(false); // Mostrar/ocultar contraseña
+  const [validationStatus, setValidationStatus] = useState({
+    length: false,
+    uppercase: false,
+    lowercase: false,
+    number: false,
+    specialChar: false
+  });
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    window.scrollTo(0, 0);
-    
-    // Validación de las contraseñas
-    if (contrasena !== confirmarContrasena) {
-      setError('Las contraseñas no coinciden');
-      window.scrollTo(0, 0);
-      return;
+  // Función para validar la contraseña
+  const validatePassword = (password) => {
+    const minLength = password.length >= 8;
+    const hasUppercase = /[A-Z]/.test(password);
+    const hasLowercase = /[a-z]/.test(password);
+    const hasNumber = /[0-9]/.test(password);
+    const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+
+    setValidationStatus({
+      length: minLength,
+      uppercase: hasUppercase,
+      lowercase: hasLowercase,
+      number: hasNumber,
+      specialChar: hasSpecialChar
+    });
+
+    // Verificar si cumple con todos los requisitos
+    if (!minLength || !hasUppercase || !hasLowercase || !hasNumber || !hasSpecialChar) {
+      //setError('La contraseña debe tener al menos 8 caracteres, una mayúscula, una minúscula, un número y un carácter especial.');
+      return false;
     }
 
-    const data = {
-      nombre: nombres,
-      apellido: apellidos,
-      correo: correo,
-      usuario: usuario,
-      tipo_usuario: 1,
-      password: contrasena,
-      password_confirmation: confirmarContrasena
-    }
-
-    try {
-      const response = await axios.post(`${API_BASE_URL}/register`, data);
-      console.log('Registro exitoso:', response.data);
-      setShowConfirmation(true); // Mostrar la alerta de confirmación
-      setError(null);
-      setTimeout(() => setRedirect(true), 2000);
-      // Puedes redirigir al usuario o mostrar un mensaje de éxito aquí
-    } catch (error) {
-      console.error('Error al registrar el docente:', error.response ? error.response.data : error.message);
-      setError('Correo o Usuario ya registrados');
-    }
-    
+    setError(null);
+    return true;
   };
 
-  if (redirect) {
-    return <Navigate to="/login" />;
-  }
+  const handlePasswordChange = (e) => {
+    const newPassword = e.target.value;
+    setContrasena(newPassword);
+    validatePassword(newPassword);
+  };
+
+  // Función para guardar un nuevo estudiante
+  const handleSave = async (e) => {
+    e.preventDefault();
+
+    try {
+      if (!nombre_user || !apellido_user || !correo) {
+        setError('Todos los campos son requeridos');
+        return;
+      }
+
+      if (contrasena_usuario !== confirmarContrasena) {
+        setError('Las contraseñas no coinciden');
+        window.scrollTo(0, 0);
+        return;
+      }
+  
+
+      const data = {
+        nombre_user,
+        apellido_user,
+        correo,
+        tipo_usuario: 1, 
+        contrasena_usuario, // Aquí enviamos la contraseña validada
+        confirmarContrasena
+
+      };
+
+      console.log('Datos enviados:', data);
+      const response = await axios.post(endPoint, data);
+
+      setExito(true);
+      setError('');
+      setNombre('');
+      setApellido('');
+      setCorreo('');
+      setContrasena('');
+    } catch (error) {
+      if (error.response) {
+        setError(`Error al registrar el estudiante: ${error.response.data.message || error.response.data}`);
+      } else {
+        setError('Error al registrar el estudiante: No se pudo conectar con el servidor.');
+      }
+      setExito(false);
+    }
+  };
 
   return (
-    <Container className="mt-5" style={{ maxWidth: '600px', backgroundColor: '#fafefe' }}>
-      <h2 className="text-center" style={{ color: '#215f88' }}>Registro de Docentes/Tutores</h2>
-      {error && <Alert variant="danger">{error}</Alert>}
-      {showConfirmation && <Alert variant="success">Registro exitoso. Redirigiendo...</Alert>}
-      <Form onSubmit={handleSubmit} style={{ backgroundColor: '#f8f9fa', padding: '20px', borderRadius: '5px' }}>
-        <Form.Group controlId="formNombres" className="mb-3">
-          <Form.Label>Nombres</Form.Label>
+    <div className="container mt-5" style={{ maxWidth: '500px', backgroundColor: '#F8F9FA', padding: '20px', borderRadius: '10px' }}>
+      <h2 className="text-center" style={{ color: '#007BFF' }}>Registro de Docente/tutor</h2>
+
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+      {exito && <p style={{ color: 'green' }}>Registro exitoso</p>}
+
+      <Form onSubmit={handleSave}>
+        <Form.Group className="mb-3" controlId="formNombre">
+          <Form.Label>Nombre</Form.Label>
           <Form.Control
             type="text"
             placeholder="Ingresa tu nombre"
-            value={nombres}
-            onChange={(e) => setNombres(e.target.value)}
-            required
+            value={nombre_user}
+            onChange={(e) => {
+              const regex = /^[a-zA-Z\s]*$/;
+              if (regex.test(e.target.value)) {
+                setNombre(e.target.value);
+              }
+            }}
           />
         </Form.Group>
-        <Form.Group controlId="formApellidos" className="mb-3">
-          <Form.Label>Apellidos</Form.Label>
+
+        <Form.Group className="mb-3" controlId="formApellido">
+          <Form.Label>Apellido</Form.Label>
           <Form.Control
             type="text"
-            placeholder="Ingresa tus apellidos"
-            value={apellidos}
-            onChange={(e) => setApellidos(e.target.value)}
-            required
+            placeholder="Ingresa tu apellido"
+            value={apellido_user}
+            onChange={(e) => {
+              const regex = /^[a-zA-Z\s]*$/;
+              if (regex.test(e.target.value)) {
+                setApellido(e.target.value);
+              }
+            }}
           />
         </Form.Group>
-        <Form.Group controlId="formCorreo" className="mb-3">
-          <Form.Label>Correo electrónico</Form.Label>
+
+        <Form.Group className="mb-3" controlId="formCorreo">
+          <Form.Label>Correo Electrónico</Form.Label>
           <Form.Control
             type="email"
-            placeholder="Ingresa tu correo electrónico"
+            placeholder="Ingresa tu correo electronico"
             value={correo}
             onChange={(e) => setCorreo(e.target.value)}
-            required
+            style={{ '::placeholder': { fontSize: '10px', color: '#999' } }}
+
           />
         </Form.Group>
+
         <Form.Group controlId="formUsuario" className="mb-3">
           <Form.Label>Usuario</Form.Label>
           <Form.Control
@@ -103,16 +164,46 @@ function RegistroDocente() {
             required
           />
         </Form.Group>
+
         <Form.Group controlId="formContrasena" className="mb-3">
           <Form.Label>Contraseña</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Ingresa tu contraseña"
-            value={contrasena}
-            onChange={(e) => setContrasena(e.target.value)}
-            required
-          />
+          <InputGroup>
+            <Form.Control
+              type={showPassword ? 'text' : 'password'}
+              placeholder="Ingresa tu contraseña"
+              value={contrasena_usuario}
+              onChange={handlePasswordChange}
+            />
+            {/* Cambiar el estilo del InputGroup.Text para que no parezca un botón */}
+            <InputGroup.Text 
+              onClick={() => setShowPassword(!showPassword)} 
+              style={{
+                backgroundColor: 'transparent',
+                border: 'none',
+                cursor: 'pointer',
+                padding: '0 10px',
+                display: 'flex',
+                alignItems: 'center'
+              }}
+            >
+              {showPassword ? <FaEyeSlash /> : <FaEye />}
+            </InputGroup.Text>
+
+          </InputGroup>
+
+          {/* Criterios de validación */}
+          <ul style={{ listStyleType: 'none', paddingLeft: '0', color: 'red' }}>
+            <li style={{ color: validationStatus.length ? 'green' : 'red' }}>Al menos 8 caracteres</li>
+            <li style={{ color: validationStatus.uppercase ? 'green' : 'red' }}>Al menos una letra mayúscula</li>
+            <li style={{ color: validationStatus.lowercase ? 'green' : 'red' }}>Al menos una letra minúscula</li>
+            <li style={{ color: validationStatus.number ? 'green' : 'red' }}>Al menos un número</li>
+            <li style={{ color: validationStatus.specialChar ? 'green' : 'red' }}>Al menos un carácter especial (@, #, $, etc.)</li>
+          </ul>
+
+          {/* Mensaje de error si la contraseña no cumple con los requisitos */}
+          {/*error && <p style={{ color: 'red' }}>{error}</p>*/}
         </Form.Group>
+
         <Form.Group controlId="formConfirmarContrasena" className="mb-3">
           <Form.Label>Confirmar Contraseña</Form.Label>
           <Form.Control
@@ -123,12 +214,13 @@ function RegistroDocente() {
             required
           />
         </Form.Group>
-        <Button type="submit" style={{ backgroundColor: '#007bff', color: 'white', width: '100%' }}>
+
+        <Button variant="primary" type="submit" className="w-100">
           Registrar
         </Button>
       </Form>
-    </Container>
+    </div>
   );
 }
 
-export default RegistroDocente;
+export default RegistroEstudiante;
