52aebf1927a2392c87d521c38d454a2f0f65f793
Boris Anthony Angulo Urquieta <borisanthony03@gmail.com>
2024-11-01 15:01:52 -0400

    cambiando el endpoint del api

--- src/components/config.js
+++ src/components/config.js
@@ -1,3 +1,3 @@
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
-
+//const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+const API_BASE_URL = "https://sigetisapi-production.up.railway.app/api"
 export { API_BASE_URL  };
